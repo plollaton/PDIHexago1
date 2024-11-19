a95a4b24db0cf26694997fd072b2df048851eee2
Pio Neto <pio.neto@db1.com.br>
2024-11-19 09:38:25 -0300

    Customer mapper to meet the account rules.

--- src/main/java/com/poc/pdihexago1/infra/CustomerEntityMapper.java
+++ src/main/java/com/poc/pdihexago1/infra/CustomerEntityMapper.java
@@ -6,6 +6,7 @@ import org.mapstruct.InjectionStrategy;
 import org.mapstruct.Mapper;
 import org.mapstruct.MappingConstants;
 
+import com.poc.pdihexago1.domains.accounts.dtos.AccountCustomerDto;
 import com.poc.pdihexago1.domains.customers.models.Customer;
 
 @Mapper(componentModel = MappingConstants.ComponentModel.SPRING,
@@ -15,4 +16,6 @@ public interface CustomerEntityMapper {
     CustomerEntity fromCustomer(Customer customer);
     Customer toCustomer(CustomerEntity customerEntity);
     List<Customer> toCustomerList(List<CustomerEntity> customersEntity);
+
+    CustomerEntity fromAccountCustomerDto(AccountCustomerDto accountCustomerDto);
 }
